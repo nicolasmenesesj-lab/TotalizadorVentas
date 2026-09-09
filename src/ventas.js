4852b70d6ed69685bc3e7af12d72d7798c0c0d02
Nico Meneses <menesesnico20@gmail.com>
2026-09-09 18:17:56 -0400

    feat: mostrar la cantidad de items ingresada

---
+++ src/ventas.js
@@ -0,0 +1,12 @@
+class Ventas {
+    getCantidad(cantidad) {
+        return cantidad;
+    }
+    getPrecio(precio) {
+        return precio;
+    }
+    calcularPrecioNeto(cantidad, precio) {
+        return cantidad * precio;
+    }
+}
+export default Ventas;
---
+++ src/ventas.spec.js
@@ -0,0 +1,8 @@
+import Ventas from './ventas.js';
+
+describe('Ventas', () => {
+    it('deberia mostrar la cantidad de items ingresada', () => {
+        const ventas = new Ventas();
+        expect(ventas.getCantidad(20)).toEqual(20);
+    });
+});
